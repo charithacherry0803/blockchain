5dbbac52b404991076d08997e3e44dbe4c05ee06
charitha <68242159+charithacherry0803@users.noreply.github.com>
2020-07-26 21:39:11 +0530

    Update reportcard.sol

--- reportcard.sol
+++ reportcard.sol
@@ -1,11 +1,4 @@
 # blockchain
-
-
-contract id: 0x8f2162ea4ef2fa538511ce77741ad7e88b5aa694
-
-Deployment id in ropsten : https://ropsten.etherscan.io/tx/0xf92b8a007807b493b3a359353344c3865bd96102c277979e89265eb9a1b22a26
-
-Solidity code : 
 pragma solidity ^0.4.17 < 0.6.12; 
 
 contract Reportcard{
